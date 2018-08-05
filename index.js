43dd549e2bc72d3d45f7f2a757a898603280c949
ronyis <ron@epsagon.com>
2018-08-05 14:22:36 +0300

    Adding files

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const symlink = require('./src/symlink');
+const path = require('path');
+
+
+class PackageExternal {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = Object.assign({
+      external: []
+    }, this.serverless.service.custom && this.serverless.service.custom.packageExternal || {});
+
+    this.symlinked = false;
+
+    this.hooks = {
+      'before:package:createDeploymentArtifacts': this.beforeDeploy.bind(this),
+      'after:deploy:deploy': this.afterDeploy.bind(this)
+    };
+
+    this.handleExit();
+  }
+
+  beforeDeploy() {
+    // Symlink external folders
+    return Promise.all(this.options.external.map(externalFolder => {
+        this.symlinked = true;
+        return symlink.createFolder(externalFolder, this.serverless);
+      }))
+      .then(() => {
+        this.serverless.cli.log(`[serverless-package-external] is complete`);
+      });
+  }
+
+  afterDeploy() {
+    if(this.symlinked) {
+      this.options.external.forEach(externalFolder => {
+        const target = path.basename(externalFolder);
+        symlink.removeFolder(target);
+      });
+    }
+  }
+
+  handleExit(func) {
+    ['SIGINT', 'SIGTERM', 'SIGQUIT']
+      .forEach(signal => process.on(signal, () => {
+        this.afterDeploy();
+      }));
+  }
+}
+
+module.exports = PackageExternal;
---
+++ src/symlink.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const yesno = require('yesno');
+
+const exists = path => {
+  try {
+    const stats = fs.lstatSync(path);
+    if(!stats.isSymbolicLink()) {
+      return true;
+    }
+  } catch(e) {}
+
+  return false;
+};
+
+const askToOverwrite = (targetExists, folder) => {
+  let ask = Promise.resolve();
+  if(targetExists.length > 0) {
+    ask = new Promise((resolve, reject) => {
+      let targets = 'Folders';
+      console.log(targetExists);
+      if(targetExists.length < 5) {
+        targets = targetExists.join(', ');
+      }
+      yesno.ask(`${targets} from ${folder} already exist${targetExists.length == 1 ? 's' : ''} in the service folder, do you want to overwrite files?`, false, ok =>
+        ok ? resolve() : reject()
+      );
+    });
+  }
+
+  return ask;
+};
+
+// Symlink a folder
+const createFolder = (folder, serverless) => {
+  const target = path.join(process.cwd(), path.basename(folder));
+
+  // Check if folder/file with symlink name already exists in top level
+  return askToOverwrite(exists(target) ? [target] : [], folder)
+    .then(() => {
+      // There is either no conflict or the user has accepted overwriting
+      serverless.cli.log(`[serverless-package-external] Symlinking ${folder}`);
+      rimraf.sync(target);
+      fs.symlinkSync(folder, target);
+    });
+};
+
+const removeFolder = folder => {
+  rimraf.sync(path.join(process.cwd(), folder));
+};
+
+module.exports = { createFolder, removeFolder };
